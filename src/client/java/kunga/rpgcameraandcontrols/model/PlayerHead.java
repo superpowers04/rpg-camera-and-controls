d7dda23b435f6fe7404e0d5f3c4234d2c5668184
Steph <38338700+superpowers04@users.noreply.github.com>
2026-01-11 22:06:48 -0500

    Forgot to commit these

--- src/client/java/kunga/rpgcameraandcontrols/model/PlayerHead.java
+++ src/client/java/kunga/rpgcameraandcontrols/model/PlayerHead.java
@@ -12,12 +12,12 @@ public final class PlayerHead {
     public static void update(double deltaSeconds) {
         var turnSpeedInDegreesPerSecond = RpgPlayerInput.getTurnSpeedInDegreesPerSecond();
         var normalizeSpeed = MathHelper.clamp(
-            turnSpeedInDegreesPerSecond / RpgConfig.instance.TURN_SPEED_PER_SEC,
+            turnSpeedInDegreesPerSecond / RpgConfig.TURN_SPEED_PER_SEC,
             -1.0,
             1.0
         );
         var targetYawRadiant = Math.toRadians(MAX_YAW_DEG) * normalizeSpeed;
-        var alpha = 1.0 - Math.exp(RpgConfig.instance.SMOOTHING_SPEED * -deltaSeconds);
+        var alpha = 1.0 - Math.exp(RpgConfig.SMOOTHING_SPEED * -deltaSeconds);
         currentYawRadiant += (float) ((targetYawRadiant - currentYawRadiant) * alpha);
     }
 
